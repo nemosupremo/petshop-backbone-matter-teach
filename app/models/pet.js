665b1011a318e0e24dfaabe263ac5dd5da8b247f
nemothekid <psnim2000@gmail.com>
2013-03-20 21:58:55 -0700

    Code clean

--- app/models/pet.js
+++ app/models/pet.js
@@ -3,11 +3,10 @@
     defaults: function() {
       return {
         image: "pet.png",
-        max_health: 10,
         health: 10,
+        max_health: 10,
       };
     },
-
     initialize: function() {
       this.on("change", _.throttle(function() {this.save()}, 250), this);
     },
